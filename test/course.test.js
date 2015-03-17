7e9ac66bf9ed4dd02ca70b383fb4760f770bbf74
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-18 00:27:58 +0800

    Add test for Course#__setField__()

--- test/course.test.js
+++ test/course.test.js
@@ -6,12 +6,38 @@ describe('Course ', function () {
     var course;
 
     beforeEach(function () {
-        course = new Course();
+        course = new Course('1234');
     });
 
     describe('#.ctor()', function () {
         it('should be able to instantiate', function () {
             assert.equal(true, !!course);
+        });
+    });
+
+    describe('#__setField__()', function () {
+        beforeEach(function () {
+            course.title = 'AAA';
+        });
+
+        it('should be able to set valid field', function () {
+            course.__setField__('title', 'BBB');
+            assert.equal(course.title, 'BBB');
+        });
+
+        it('should not set the field null', function () {
+            course.__setField__('title', null);
+            assert.equal(course.title, 'AAA');
+        });
+
+        it('should not set the field undefined', function () {
+            course.__setField__('title', undefined);
+            assert.equal(course.title, 'AAA');
+        });
+
+        it('should not set the field empty', function () {
+            course.__setField__('title', '');
+            assert.equal(course.title, 'AAA');
         });
     });
 
