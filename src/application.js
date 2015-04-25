52e5e8a3aa476bb453a68833b806f8b2266e8800
Trotyl Yu <yzj1995@vip.qq.com>
2015-04-26 02:21:57 +0800

    Refactor application.js

--- src/application.js
+++ src/application.js
@@ -28,20 +28,21 @@ Application.prototype.identify = function (id, password, callback) {
     var self = this;
     var user;
 
-    if (this._users_[id]) {                             // 若该用户已存在，则从用户集合中直接取出
+    if (this._users_[id]) {                                 // 若该用户已存在，则从用户集合中直接取出
         user = this._users_[id];
     }
-    else {                                              // 若该用户不存在，则创建该用户
+    else {                                                  // 若该用户不存在，则创建该用户
         user = new User(id, password, self);
-        this._users_[user._id_] = user;                 // 将该用户添加到用户集合中
+        this._users_[user._id_] = user;                     // 将该用户添加到用户集合中
     }
 
-    user.__ensureLogin__().then(function () {           // 确保处于登录状态
-        self._current_ = user;                          // 若登录成功则将该用户设为应用的当前用户
-        return user;
-    }).nodeify(function (err, res) {                    // 执行回调函数
-        _.isFunction(callback) && callback(err, res);
-    });
+    user.__ensureLogin__()                                  // 确保处于登录状态
+        .then(function () {
+            self._current_ = user;                          // 若登录成功则将该用户设为应用的当前用户
+            return user;
+        }).nodeify(function (err, res) {                    // 执行回调函数
+            _.isFunction(callback) && callback(err, res);
+        });
 
     return user;
 };
@@ -55,24 +56,26 @@ Application.prototype.reset = function () {
 // 课程搜索方法
 Application.prototype.searchForCourses = function (options, callback) {
     var self = this;
-    return self.__searchForCoursesOnline__(options).then(null, function (err) { // 尝试在线获取
-        return self.__searchForCoursesOffline__(options);                       // 若在线获取失败则离线获取
-    }).nodeify(function (err, res) {                                            // 执行回调函数
-        _.isFunction(callback) && callback(err, res);
-    });
+    return self.__searchForCoursesOnline__(options)
+        .then(null, function () {                               // 尝试在线获取
+            return self.__searchForCoursesOffline__(options);   // 若在线获取失败则离线获取
+        }).nodeify(function (err, res) {                        // 执行回调函数
+            _.isFunction(callback) && callback(err, res);
+        });
 };
 
 // 人员搜索方法
 Application.prototype.searchForPeople = function (term, limit, callback) {
     var self = this;
-    if (!limit || limit <= 0) {     // 若限制参数为空则设为10
+    if (!limit || limit <= 0) {                                     // 若限制参数为空则设为10
         limit = 10;
     }
-    return self.__searchForPeopleOnline__(term, limit).then(null, function (err) {  // 尝试在线获取
-        return self.__searchForPeopleOffline__(term, limit);                        // 若在线获取失败则离线获取
-    }).nodeify(function (err, res) {                                                // 执行回调函数
-        _.isFunction(callback) && callback(err, res);
-    });
+    return self.__searchForPeopleOnline__(term, limit)
+        .then(null, function () {                                   // 尝试在线获取
+            return self.__searchForPeopleOffline__(term, limit);    // 若在线获取失败则离线获取
+        }).nodeify(function (err, res) {                            // 执行回调函数
+            _.isFunction(callback) && callback(err, res);
+        });
 };
 
 
@@ -83,10 +86,11 @@ Application.prototype.searchForPeople = function (term, limit, callback) {
 Application.prototype.__broke__ = function (number, password) {
     var self = this;
     var user = new User(number, password);
-    return user.__login__().then(function () {  // 登录用户
-        self._current_ = user;
-        return user;
-    });
+    return user.__login__()                 // 登录用户
+        .then(function () {
+            self._current_ = user;
+            return user;
+        });
 };
 
 // 课程缓存方法
@@ -130,32 +134,37 @@ Application.prototype.__searchForCoursesOnline__ = function (options) {
     var self = this;
     var getMeta = UrlUtil.getAppSearchCoursesPreMeta(this._current_);       // 获取课程搜索准备参数
     var postMeta = UrlUtil.getAppSearchCoursesMeta(this._current_, options);// 获取课程搜索参数
-    return self._current_.__ensureLogin__().then(function () {              // 确保登录状态
-        return Carrier.get(getMeta).then(function (getRes) {                // 发送课程搜索准备请求
-            return Carrier.post(postMeta).then(function (postRes) {         // 发送课程搜索请求
-                return Parser.getAppCourses(postRes.body);                  // 解析响应内容
-            }).then(self.__cacheCourses__);                                 // 缓存课程列表
+    return self._current_.__ensureLogin__()
+        .then(function () {                                                 // 确保登录状态
+            return Carrier.get(getMeta);                                    // 发送课程搜索准备请求
+        }).then(function () {
+            return Carrier.post(postMeta);                                  // 发送课程搜索请求
+        }).then(function (postRes) {
+            return Parser.getAppCourses(postRes.body);                      // 解析响应内容
+        }).then(function (courses) {
+            return self.__cacheCourses__(courses);                          // 缓存课程列表
         });
-    });
 };
 
 // 离线人员搜索方法
 Application.prototype.__searchForPeopleOffline__ = function (term, limit) {
     var people = [];
-    _.forEach(this._people_, function (person) {    // 遍历人员列表本地缓存
+    for(var id in this._people_) {
+        var person = this._people_[id];
         var flag = false;
-        _.forEach(person, function (val, key) {     // 遍历人员参数
+        for(var key in person) {            // 遍历人员参数
+            var val = person[key];
             if (val &&
-                people.length <= limit &&           // 确保数量上限
-                !_.startsWith(key, '_') &&          // 排除私有字段
-                val.indexOf(term) >= 0) {           // 判断当前参数是否符合
-                    flag = true;
+                people.length <= limit &&   // 确保数量上限
+                !_.startsWith(key, '_') &&  // 排除私有字段
+                val.indexOf(term) >= 0) {   // 判断当前参数是否符合
+                flag = true;
             }
-        });
-        if (flag) {                                 // 若某个参数符合则加入该人员
+        }
+        if (flag) {                         // 若某个参数符合则加入该人员
             people.push(person);
         }
-    });
+    }
     return Promise.resolve(people);
 };
 
@@ -164,11 +173,12 @@ Application.prototype.__searchForPeopleOnline__ = function (term, limit) {
     var self = this;
     var preMeta = UrlUtil.getAppSearchPeoplePreMeta(this._current_);        // 获取人员搜索准备参数
     var meta = UrlUtil.getAppSearchPeopleMeta(this._current_, term, limit); // 获取人员搜索参数
-    return self._current_.__ensureLogin__().then(function () {              // 确保登录状态
-        return Carrier.post(preMeta);                                       // 发送人员搜索准备请求
-    }).then(function () {
-        return Carrier.post(meta);                                          // 发送人员搜索请求
-    }).then(function (res) {
-        return JSON.parse(res.body).principals;                             // 解析响应内容
-    });
+    return self._current_.__ensureLogin__()                                 // 确保登录状态
+        .then(function () {
+            return Carrier.post(preMeta);                                   // 发送人员搜索准备请求
+        }).then(function () {
+            return Carrier.post(meta);                                      // 发送人员搜索请求
+        }).then(function (res) {
+            return JSON.parse(res.body).principals;                         // 解析响应内容
+        });
 };
