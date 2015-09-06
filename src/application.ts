660a8d9cfcd2b6d2894fd120df44b3cfd35e396b
Trotyl Yu <yzj1995@vip.qq.com>
2015-09-07 01:14:52 +0800

    Add docs for application.ts.

--- src/application.ts
+++ src/application.ts
@@ -22,20 +22,29 @@ import { Seeker } from './helpers/seeker';
 
 import { ISearchCoursesOption, ISearchPeopleOption } from './utils/interfaces';
 
-
+/** @unaccessible Dependency instance. */
 var cacher: Cacher = injector.get('Cacher');
+/** @unaccessible Dependency instance. */
 var caller: Caller = injector.get('Caller');
+/** @unaccessible Dependency instance. */
 var fetcher: Fetcher = injector.get('Fetcher');
+/** @unaccessible Dependency instance. */
 var seeker: Seeker = injector.get('Seeker');
 
+/** @unaccessible Dependency instance. */
 var userFactory: UserFactory = injector.get('UserFactory');
+/** @unaccessible Dependency instance. */
 var exceptionFactory: ExceptionFactory = injector.get('ExceptionFactory');
 
 /** 
  * @class
- * Represents an instance of SDK application.
+ * Represents an instance of SDK application. The entry of the entire library.
  */
 export class Application {
+    /**
+     * @property
+     * The user instance for application global operations.
+     */
     private currentUser: User;
     
     /**
@@ -46,14 +55,32 @@ export class Application {
         this.currentUser = _.find(cacher.users, (user) => user.isConfirmed) || null;
     }
     
+    /**
+     * @method
+     * Get the Injector instance of current application.
+     * @returns the Injector instance of current application.
+     */
     getInjector (): Injector {
         return injector;
     }
     
+    /**
+     * @method
+     * Get the exact user of given student id which has been registered before.
+     * @param id The student id of the student.
+     * @returns The student instance if exist, null if not.
+     */
     one (id: string): User {
         return cacher.users[id] || null;
     }
 
+    /**
+     * @method
+     * Register a student entity with student id and password.
+     * @param id The student id of the student.
+     * @param password The password of the UESTC Portal site.
+     * @returns The student instance of the given student id.
+     */
     register (id: string, password: string): User {
         var user: User;
         cacher.users[id] = user = userFactory.$new(id, password);
@@ -64,7 +91,14 @@ export class Application {
         return user;
     }
     
-    searchForCourses (option: ISearchCoursesOption, callback?: { (error: Error, courses: Course[]): void; }): Observable<Course[]> {       
+    /**
+     * @method
+     * Search for courses that satisfy the given option online.
+     * @param option The option of the search.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The Observable instance of the search result.
+     */
+    searchForCourses (option: ISearchCoursesOption, callback?: { (error: Exception, courses: Course[]): void; }): Observable<Course[]> {       
         var observable = Observable.create<Course[]>((observer) => {
             if(!this.isUserExist()) {
                 observer.onError(exceptionFactory.$new(401, 'Application#searchForCourses must be called with a current user.'));
@@ -74,20 +108,41 @@ export class Application {
         return observable;
     }
     
-    searchForCoursesInCache (option: ISearchCoursesOption, callback?: { (error: Error, courses: Course[]): void; }): Observable<Course[]> {
+    /**
+     * @method
+     * Search for courses that satisfy the given option offline.
+     * @param option The option of the search.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The Observable instance of the search result.
+     */
+    searchForCoursesInCache (option: ISearchCoursesOption, callback?: { (error: Exception, courses: Course[]): void; }): Observable<Course[]> {
         var observable = seeker.searchForCourses(option);
         caller.nodifyObservable(observable, callback);
         return observable;
     }
     
-    searchForCoursesWithCache (option: ISearchCoursesOption, callback?: { (error: Error, courses: Course[]): void; }): Observable<Course[]> {
+    /**
+     * @method
+     * Search for courses that satisfy the given option online when possible, offline when necessary.
+     * @param option The option of the search.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The Observable instance of the search result.
+     */
+    searchForCoursesWithCache (option: ISearchCoursesOption, callback?: { (error: Exception, courses: Course[]): void; }): Observable<Course[]> {
         var observable = this.searchForCourses(option)
             .catch(this.searchForCoursesInCache(option)); 
         caller.nodifyObservable(observable, callback);
         return observable;
     }
     
-    searchForPeople (option: ISearchPeopleOption, callback?: { (error: Error, people: Person[]): void; }): Observable<Person[]> {       
+    /**
+     * @method
+     * Search for people that satisfy the given option online.
+     * @param option The option of the search.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The Observable instance of the search result.
+     */
+    searchForPeople (option: ISearchPeopleOption, callback?: { (error: Exception, people: Person[]): void; }): Observable<Person[]> {       
         var observable = Observable.create<Person[]>((observer) => {
             if(!this.isUserExist()) {
                 observer.onError(exceptionFactory.$new(401, 'Application#searchForPeople must be called with a current user.'));
@@ -97,29 +152,60 @@ export class Application {
         return observable;
     }
     
-    searchForPeopleInCache (option: ISearchPeopleOption, callback?: { (error: Error, people: Person[]): void; }): Observable<Person[]> {
+    /**
+     * @method
+     * Search for people that satisfy the given option offline.
+     * @param option The option of the search.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The Observable instance of the search result.
+     */
+    searchForPeopleInCache (option: ISearchPeopleOption, callback?: { (error: Exception, people: Person[]): void; }): Observable<Person[]> {
         var observable = seeker.searchForPeople(option);
         caller.nodifyObservable(observable, callback);
         return observable;
     }
     
-    searchForPeopleWithCache (option: ISearchPeopleOption, callback?: { (error: Error, people: Person[]): void; }): Observable<Person[]> {
+    /**
+     * @method
+     * Search for people that satisfy the given option online when possible, offline when necessary.
+     * @param option The option of the search.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The Observable instance of the search result.
+     */
+    searchForPeopleWithCache (option: ISearchPeopleOption, callback?: { (error: Exception, people: Person[]): void; }): Observable<Person[]> {
         var observable = this.searchForPeople(option)
             .catch(this.searchForPeopleInCache(option));   
         caller.nodifyObservable(observable, callback);
         return observable;
     }
     
-    verify (id: string, password: string, callback?: { (error: Error, result: boolean): void; }): Observable<boolean> {
+    /**
+     * @method
+     * Verify whether the given student id and password is valid. Notice that if some other accident occurs (i.e. No network), the result would be null, please check the error.
+     * @param id The student id of the student.
+     * @param password The password of the student.
+     * @param callback The function to be called with an error or the result of search if don't want to use the Observable operations. It's deprecated.
+     * @returns The boolean result of valid or not. 
+     */
+    verify (id: string, password: string, callback?: { (error: Exception, result: boolean): void; }): Observable<boolean> {
         var user = new User(id, password);
         var observable = user.confirm();
         caller.nodifyObservable(observable, callback);
         return observable;
     }
     
+    /**
+     * @method
+     * Check whether there is a user to deal with global operation.
+     * @returns The boolean result of exist or not.
+     */
     private isUserExist (): boolean {
         return !!this.currentUser;
     }
 }
 
+/**
+ * @instance
+ * The Application instance for quick access.
+ */
 export const app: Application = new Application();
